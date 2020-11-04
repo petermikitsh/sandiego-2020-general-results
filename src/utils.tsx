896f2ec6ed0a36c9105a60e779052236b84e48d1
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-11-03 23:42:41 -0800

    chore: bust cache

--- src/utils.tsx
+++ src/utils.tsx
@@ -13,7 +13,7 @@ export type ContestSummary = {
 
 async function getFile<T>(file: string) {
   const result: T = await (
-    await fetch(`/sandiego-2020-general-results/data/${file}?v=8`)
+    await fetch(`/sandiego-2020-general-results/data/${file}?v=9`)
   ).json();
   return result;
 }
