9517a13f8cc864b4ee34fe0596ebad4e44f857b9
Peter Mikitsh <pmikitsh@zuora.com>
2021-09-15 09:08:30 -0700

    chore: add new data tranche

--- src/utils.tsx
+++ src/utils.tsx
@@ -13,7 +13,7 @@ export type ContestSummary = {
 
 async function getFile<T>(file: string) {
   const result: T = await (
-    await fetch(`/sandiego-2020-general-results/data/${file}?v=18`)
+    await fetch(`/sandiego-2020-general-results/data/${file}?v=19`)
   ).json();
   return result;
 }
